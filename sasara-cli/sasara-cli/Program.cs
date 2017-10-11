fb6bf0a2b10f37938b3c3760350d4e89cd985d63
boxp <tiyotiyouda@gmail.com>
2017-10-12 02:21:04 +0900

    Add command-line tool

---
+++ sasara-cli/sasara-cli/Program.cs
@@ -0,0 +1,32 @@
+﻿using CeVIO.Talk.RemoteService;
+
+namespace sasara_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceControl.StartHost(false);
+
+            Talker talker = new Talker();
+
+            // キャスト設定
+            talker.Cast = "さとうささら";
+
+            // （例）音量設定
+            talker.Volume = 100;
+
+            // 再生する文字列を取得
+            if (args.Length == 0 || args[0].Length == 0)
+            {
+                return;
+            }
+            string text = args[0];
+
+            // 音声ファイルを保存
+            if (!talker.OutputWaveToFile(text, "C:/WINDOWS/TEMP/sasara_output.wav")) {
+                throw new System.InvalidOperationException();
+            }
+        }
+    }
+}
