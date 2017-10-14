1e91e99e16b85a7556a7a45e202cc38fac1c6676
boxp <tiyotiyouda@gmail.com>
2017-10-15 02:58:12 +0900

    Fix cloud-storage-component

--- src/sasara_agent/infra/datasource/cloud_storage.clj
+++ src/sasara_agent/infra/datasource/cloud_storage.clj
@@ -1,23 +1,44 @@
 (ns sasara-agent.infra.datasource.cloud-storage
-  (:import (com.google.auth.oauth2 ServiceAccountCredentials)
-           (com.google.cloud.storage Bucket BucketInfo)
-           (com.google.cloud.storage Storage StorageOptions Storage$BucketGetOption))
-  (:require [clojure.spec.alpha :as s]
+  (:import (java.io InputStream)
+           (com.google.auth.oauth2 ServiceAccountCredentials)
+           (com.google.cloud.storage Acl Acl$User Acl$Role Storage StorageOptions Storage$BucketGetOption Storage$BlobTargetOption Bucket BucketInfo BlobInfo))
+  (:require [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]
             [com.stuartsierra.component :as component]))
 
 (s/def ::bucket-name string?)
-(s/def ::storage #(instance? Storage %))
+(s/def ::file-name string?)
+(s/def ::storage
+  (s/with-gen (s/nilable #(instance? Storage %))
+    (fn [] (gen/fmap (fn [_] (-> (StorageOptions/getDefaultInstance) .getService))
+                     (s/gen int?)))))
+(s/def ::file
+  (s/with-gen #(instance? (-> "" .getBytes class) %)
+    (fn [] (gen/fmap (fn [s] (-> s .getBytes))
+                     (s/gen string?)))))
 (s/def ::cloud-storage-component
-  (s/keys :req [::bucket-name]
-          :opt [::storage]))
+  (s/keys :req-un [::bucket-name ::storage]))
 
+(s/def ::upload-file-opts
+  (s/keys :req-un [::file ::file-name]))
 (s/fdef upload-file
-  :args (s/cat :c ::cloud-storage-component)
-  :ret nil?)
+  :args (s/cat :c ::cloud-storage-component
+               :opts ::upload-file-opts)
+  :ret string?)
 (defn upload-file
-  [{:keys [storage bucket-name] :as c}])
+  [{:keys [storage bucket-name] :as c}
+   {:keys [file file-name] :as opts}]
+  (-> (.create storage
+        (-> (BlobInfo/newBuilder bucket-name file-name)
+            (.setAcl [(Acl/of (Acl$User/ofAllUsers) Acl$Role/READER)])
+            .build)
+        file
+        (into-array Storage$BlobTargetOption []))
+      .getMediaLink))
 
 (s/fdef get-storage
+  :args (s/cat)
   :ret ::storage)
 (defn get-storage []
   (-> (StorageOptions/getDefaultInstance)
@@ -26,14 +47,17 @@
 (defrecord CloudStorageComponent [bucket-name storage]
   component/Lifecycle
   (start [this]
-    (println ";; Starting PubSubSubscriptionComponent")
+    (println ";; Starting CloudStorageComponent")
     (-> this
         (assoc :storage (get-storage))))
   (stop [this]
-    (println ";; Stopping PubSubSubscriptionComponent")
+    (println ";; Stopping CloudStorageComponent")
     (-> this
         (dissoc :storage))))
 
+(s/fdef cloud-storage-component
+  :args (s/cat :bucket-name ::bucket-name)
+  :ret ::cloud-storage-component)
 (defn cloud-storage-component
   [bucket-name]
   (map->CloudStorageComponent {:bucket-name bucket-name}))
