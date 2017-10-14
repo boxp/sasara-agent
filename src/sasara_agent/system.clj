883a8cc5fd6f4a4a2e978d7641fc79dcade01a45
boxp <tiyotiyouda@gmail.com>
2017-10-15 03:01:54 +0900

    Fix system-component

--- src/sasara_agent/system.clj
+++ src/sasara_agent/system.clj
@@ -2,26 +2,22 @@
   (:require [com.stuartsierra.component :as component]
             [environ.core :refer [env]]
             [sasara-agent.app.agent :refer [app-agent-component]]
-            [sasara-agent.infra.datasource.example :refer [example-datasource-component]]
+            [sasara-agent.infra.datasource.cloud-storage :refer [cloud-storage-component]]
+            [sasara-agent.infra.datasource.shell :refer [shell-component]]
             [sasara-agent.infra.repository.example :refer [example-repository-component]]
             [sasara-agent.domain.usecase.example :refer [example-usecase-component]])
   (:gen-class))
 
 (defn sasara-agent-system
-  [{:keys [sasara-agent-example-port
-           sasara-agent-my-webapp-port] :as conf}]
+  [{:keys [sasara-agent-bucket-name
+           sasara-agent-command-path] :as conf}]
   (component/system-map
-    :example-datasource (example-datasource-component sasara-agent-example-port)
-    :example-repository (component/using
-                          (example-repository-component)
-                          [:example-datasource])
-    :example-usecase (component/using
-                       (example-usecase-component)
-                       [:example-repository])))
+    :cloud-storage-datasource (cloud-storage-component sasara-agent-bucket-name)
+    :shell-datasource (shell-component sasara-agent-command-path)))
 
 (defn load-config []
-  {:sasara-agent-example-port (-> (or (env :sasara-agent-example-port) "8000") Integer/parseInt)
-   :sasara-agent-my-webapp-port (-> (or (env :sasara-agent-my-webapp-port) "8080") Integer/parseInt)})
+  {:sasara-agent-bucket-name (env :sasara-agent-bucket-name)
+   :sasara-agent-command-path (env :sasara-agent-command-path)})
 
 (defn -main []
   (component/start
